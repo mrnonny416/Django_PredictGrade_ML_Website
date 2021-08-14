7f800901d39fe301c653eec92190f3ce6e1e3c62
mrnonny416 <mrnonny416@gmail.com>
2021-08-14 16:11:21 +0700

    Edit select page and session:studentID

--- predictApp/views.py
+++ predictApp/views.py
@@ -3,13 +3,16 @@ from .loginforms import loginForm
 # Create your views here.
 def login(request):
     if request.method == 'POST':
-        print(request.POST)
         department = (request.POST.get('studentID'))[2:8]
         if department == '523206':
+            request.session['studentID'] = request.POST.get('studentID')
             return redirect('select')
     return render(request, 'login.html', {'form': loginForm})
 
 def select(request):
+    studentID = request.session.get('studentID')
+    if(studentID == None):
+        return redirect('login')
     return render(request, 'select.html')
 
 def forms(request):
