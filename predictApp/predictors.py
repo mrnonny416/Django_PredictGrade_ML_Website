d8f9f21df07a72226e980a2b3fa9bcf4bf219175
mrnonny416 <mrnonny416@gmail.com>
2021-08-28 08:24:06 +0700

    Deploy all model

--- predictApp/predictors.py
+++ predictApp/predictors.py
@@ -1,6 +1,111 @@
-
-def predict_ENGCE101(ENGCC304,ENGCC304_Instructor,FUNMA105,FUNSC101,GEBLC103):
+def predict_ENGCE101(ENGCC304,FUNMA105,FUNSC101,GEBLC103):
     import pickle
-    predictor = pickle.load(open('predictApp\predictModel_ENGCE101.p','rb'))
-    Predict_result = predictor.predict_proba([[ENGCC304,ENGCC304_Instructor,FUNMA105,FUNSC101,GEBLC103]])
+    file = open('predictApp\predictModel_ENGCE101.p','rb')
+    predictor = pickle.load(file)
+    Predict_result = predictor.predict_proba([[ENGCC304,FUNMA105,FUNSC101,GEBLC103]])
+    file.close()
+    return Predict_result[0][1]*100
+
+def predict_ENGCE102(ENGCC304,FUNMA105,FUNSC101,GEBLC103):
+    import pickle
+    file = open('predictApp\predictModel_ENGCE102.p','rb')
+    predictor = pickle.load(file)
+    Predict_result = predictor.predict_proba([[ENGCC304,FUNMA105,FUNSC101,GEBLC103]])
+    file.close()
+    return Predict_result[0][1]*100
+
+def predict_ENGCE103(ENGCC304,ENGEE106,FUNSC101,GEBLC103):
+    import pickle
+    file = open('predictApp\predictModel_ENGCE103.p','rb')
+    predictor = pickle.load(file)
+    Predict_result = predictor.predict_proba([[ENGCC304,ENGEE106,FUNSC101,GEBLC103]])
+    file.close()
+    return Predict_result[0][1]*100
+
+def predict_ENGCE104(ENGCE102,ENGCE106,ENGEE106,FUNSC101):
+    import pickle
+    file = open('predictApp\predictModel_ENGCE104.p','rb')
+    predictor = pickle.load(file)
+    Predict_result = predictor.predict_proba([[ENGCE102,ENGCE106,ENGEE106,FUNSC101]])
+    file.close()
+    return Predict_result[0][1]*100
+
+def predict_ENGCE105(ENGCE102,ENGCE103,ENGCE106,ENGEL106,FUNMA105,FUNSC101):
+    import pickle
+    file = open('predictApp\predictModel_ENGCE105.p','rb')
+    predictor = pickle.load(file)
+    Predict_result = predictor.predict_proba([[ENGCE102,ENGCE103,ENGCE106,ENGEL106,FUNMA105,FUNSC101]])
+    file.close()
+    return Predict_result[0][1]*100
+
+def predict_ENGCE106(ENGCE104,ENGEL106,FUNSC101,GEBLC103):
+    import pickle
+    file = open('predictApp\predictModel_ENGCE106.p','rb')
+    predictor = pickle.load(file)
+    Predict_result = predictor.predict_proba([[ENGCE104,ENGEL106,FUNSC101,GEBLC103]])
+    file.close()
+    return Predict_result[0][1]*100
+
+def predict_ENGCE107(ENGCE102,ENGCE104,ENGCE106,FUNSC101):
+    import pickle
+    file = open('predictApp\predictModel_ENGCE107.p','rb')
+    predictor = pickle.load(file)
+    Predict_result = predictor.predict_proba([[ENGCE102,ENGCE104,ENGCE106,FUNSC101]])
+    file.close()
+    return Predict_result[0][1]*100
+
+def predict_ENGCE108(ENGCE105,ENGCE111,ENGCE112):
+    import pickle
+    file = open('predictApp\predictModel_ENGCE108.p','rb')
+    predictor = pickle.load(file)
+    Predict_result = predictor.predict_proba([[ENGCE105,ENGCE111,ENGCE112]])
+    file.close()
+    return Predict_result[0][1]*100
+
+def predict_ENGCE109(ENGCE104,ENGCE106,ENGEL106):
+    import pickle
+    file = open('predictApp\predictModel_ENGCE109.p','rb')
+    predictor = pickle.load(file)
+    Predict_result = predictor.predict_proba([[ENGCE104,ENGCE106,ENGEL106]])
+    file.close()
+    return Predict_result[0][1]*100
+
+def predict_ENGCE110(ENGCC304,ENGEL105,ENGEL106):
+    import pickle
+    file = open('predictApp\predictModel_ENGCE110.p','rb')
+    predictor = pickle.load(file)
+    Predict_result = predictor.predict_proba([[ENGCC304,ENGEL105,ENGEL106]])
+    file.close()
+    return Predict_result[0][1]*100
+
+def predict_ENGCE111(ENGCE103,ENGCE106,ENGEL106,GEBLC103):
+    import pickle
+    file = open('predictApp\predictModel_ENGCE111.p','rb')
+    predictor = pickle.load(file)
+    Predict_result = predictor.predict_proba([[ENGCE103,ENGCE106,ENGEL106,GEBLC103]])
+    file.close()
+    return Predict_result[0][1]*100
+
+def predict_ENGCE112(ENGCC304,ENGCE102,GEBLC103):
+    import pickle
+    file = open('predictApp\predictModel_ENGCE112.p','rb')
+    predictor = pickle.load(file)
+    Predict_result = predictor.predict_proba([[ENGCC304,ENGCE102,GEBLC103]])
+    file.close()
+    return Predict_result[0][1]*100
+
+def predict_ENGEL105(ENGCE106,ENGCE110,ENGEL106,FUNSC101,GEBLC103):
+    import pickle
+    file = open('predictApp\predictModel_ENGEL105.p','rb')
+    predictor = pickle.load(file)
+    Predict_result = predictor.predict_proba([[ENGCE106,ENGCE110,ENGEL106,FUNSC101,GEBLC103]])
+    file.close()
+    return Predict_result[0][1]*100
+
+def predict_ENGEL106(ENGCE102,ENGCE106,ENGEE106,FUNMA105,FUNSC101):
+    import pickle
+    file = open('predictApp\predictModel_ENGEL106.p','rb')
+    predictor = pickle.load(file)
+    Predict_result = predictor.predict_proba([[ENGCE102,ENGCE106,ENGEE106,FUNMA105,FUNSC101]])
+    file.close()
     return Predict_result[0][1]*100
--- predictApp/views.py
+++ predictApp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect, render
 from .loginforms import loginForm
 from .models import Subject ,Subject_refer ,Instructor
-from .predictors import predict_ENGCE101
+from .predictors import predict_ENGCE101,predict_ENGCE102,predict_ENGCE103,predict_ENGCE104,predict_ENGCE105,predict_ENGCE106,predict_ENGCE107,predict_ENGCE108,predict_ENGCE109,predict_ENGCE110,predict_ENGCE111,predict_ENGCE112,predict_ENGEL105,predict_ENGEL106
+
 # Create your views here.
 def login(request):
     if request.method == 'POST':
@@ -23,24 +24,22 @@ def forms(request):
         subjectID = request.GET.get('subjectID')
         subject_info = Subject.objects.all()
         subject_refer_info = Subject_refer.objects.filter(subjectID=(subjectID))
-        Instructor_info = Instructor.objects.all()
+
         for subject_info_roll in subject_info:
             for subject_refer_info_roll in subject_refer_info:
                 if(subject_info_roll.subjectID == subject_refer_info_roll.ref_subjectID):
                     subject_refer_index.append(subject_info_roll.subjectID)
                     break
-    return render(request, 'forms.html' ,{'subjectID':subjectID ,'subject_info':subject_info ,'subject_refer_info':subject_refer_info ,'Instructor_info':Instructor_info,'subject_refer_index':subject_refer_index})
+    return render(request, 'forms.html' ,{'subjectID':subjectID ,'subject_info':subject_info ,'subject_refer_info':subject_refer_info ,'subject_refer_index':subject_refer_index})
 
 def reports(request):
     if request.method == 'POST':
         Instructor_number = []
-        InstructorID = []
-        Grade = []
+        GradeRoll = []
         subject_refer_index = []
         subjectID = request.GET.get('subjectID')
         subject_info = Subject.objects.all()
         subject_refer_info = Subject_refer.objects.filter(subjectID=(subjectID))
-        Instructor_info = Instructor.objects.all()
         for subject_info_roll in subject_info:
             for subject_refer_info_roll in subject_refer_info:
                 if(subject_info_roll.subjectID == subject_refer_info_roll.ref_subjectID):
@@ -57,15 +56,93 @@ def reports(request):
             elif grade_roll == '1':Grade = 'D'
             elif grade_roll == '0':Grade = 'F'
             else:Grade = 'ยังไม่ได้ลงทะเบียนเรียนหรือถอนรายวิชา'
-            InstructorID.append([subject_refer_index_roll,request.POST.get('instructorID_'+subject_refer_index_roll),Grade])
+            GradeRoll.append([subject_refer_index_roll,Grade])
 
-        for i in InstructorID:
-            Instructor_number.append(int((i[1])[3:5]))
         if subjectID == 'ENGCE101':
-            Predict_result = prediction(float(request.POST.get('grade_ENGCC304')),Instructor_number[2],float(request.POST.get('grade_FUNMA105')),float(request.POST.get('grade_FUNSC101')),float(request.POST.get('grade_GEBLC103')))
-    return render(request, 'reports.html', {'subjectID':subjectID ,'subject_info':subject_info ,'Instructor_info':Instructor_info,'subject_refer_index':subject_refer_index,'InstructorID':InstructorID,'Predict_result':Predict_result})
+            Predict_result = prediction_ENGCE101(float(request.POST.get('grade_ENGCC304')),float(request.POST.get('grade_FUNMA105')),float(request.POST.get('grade_FUNSC101')),float(request.POST.get('grade_GEBLC103')))
+        elif subjectID == 'ENGCE102':
+            Predict_result = prediction_ENGCE102(float(request.POST.get('grade_ENGCC304')),float(request.POST.get('grade_FUNMA105')),float(request.POST.get('grade_FUNSC101')),float(request.POST.get('grade_GEBLC103')))
+        elif subjectID == 'ENGCE103':
+            Predict_result = prediction_ENGCE103(float(request.POST.get('grade_ENGCC304')),float(request.POST.get('grade_ENGEE106')),float(request.POST.get('grade_FUNSC101')),float(request.POST.get('grade_GEBLC103')))
+        elif subjectID == 'ENGCE104':
+            Predict_result = prediction_ENGCE104(float(request.POST.get('grade_ENGCE102')),float(request.POST.get('grade_ENGCE106')),float(request.POST.get('grade_ENGEE106')),float(request.POST.get('grade_FUNSC101')))
+        elif subjectID == 'ENGCE105':
+            Predict_result = prediction_ENGCE105(float(request.POST.get('grade_ENGCE102')),float(request.POST.get('grade_ENGCE103')),float(request.POST.get('grade_ENGCE106')),float(request.POST.get('grade_ENGEL106')),float(request.POST.get('grade_FUNMA105')),float(request.POST.get('grade_FUNSC101')))
+        elif subjectID == 'ENGCE106':
+            Predict_result = prediction_ENGCE106(float(request.POST.get('grade_ENGCE104')),float(request.POST.get('grade_ENGEL106')),float(request.POST.get('grade_FUNSC101')),float(request.POST.get('grade_GEBLC103')))
+        elif subjectID == 'ENGCE107':
+            Predict_result = prediction_ENGCE107(float(request.POST.get('grade_ENGCE102')),float(request.POST.get('grade_ENGCE104')),float(request.POST.get('grade_ENGCE106')),float(request.POST.get('grade_FUNSC101')))
+        elif subjectID == 'ENGCE108':
+            Predict_result = prediction_ENGCE108(float(request.POST.get('grade_ENGCE105')),float(request.POST.get('grade_ENGCE111')),float(request.POST.get('grade_ENGCE112')))
+        elif subjectID == 'ENGCE109':
+            Predict_result = prediction_ENGCE109(float(request.POST.get('grade_ENGCE104')),float(request.POST.get('grade_ENGCE106')),float(request.POST.get('grade_ENGEL106')))
+        elif subjectID == 'ENGCE110':
+            Predict_result = prediction_ENGCE110(float(request.POST.get('grade_ENGCC304')),float(request.POST.get('grade_ENGEL105')),float(request.POST.get('grade_ENGEL106')))
+        elif subjectID == 'ENGCE111':
+            Predict_result = prediction_ENGCE111(float(request.POST.get('grade_ENGCE103')),float(request.POST.get('grade_ENGCE106')),float(request.POST.get('grade_ENGEL106')),float(request.POST.get('grade_GEBLC103')))
+        elif subjectID == 'ENGCE112':
+            Predict_result = prediction_ENGCE112(float(request.POST.get('grade_ENGCC304')),float(request.POST.get('grade_ENGCE102')),float(request.POST.get('grade_GEBLC103')))
+        elif subjectID == 'ENGEL105':
+            Predict_result = prediction_ENGEL105(float(request.POST.get('grade_ENGCE106')),float(request.POST.get('grade_ENGCE110')),float(request.POST.get('grade_ENGEL106')),float(request.POST.get('grade_FUNSC101')),float(request.POST.get('grade_GEBLC103')))
+        elif subjectID == 'ENGEL106':
+            Predict_result = prediction_ENGEL106(float(request.POST.get('grade_ENGCE102')),float(request.POST.get('grade_ENGCE106')),float(request.POST.get('grade_ENGEE106')),float(request.POST.get('grade_FUNMA105')),float(request.POST.get('grade_FUNSC101')))
 
 
-def prediction(ENGCC304,ENGCC304_Instructor,FUNMA105,FUNSC101,GEBLC103):
-    Predict_result = predict_ENGCE101(ENGCC304,ENGCC304_Instructor,FUNMA105,FUNSC101,GEBLC103)
+    return render(request, 'reports.html', {'subjectID':subjectID ,'subject_info':subject_info ,'subject_refer_index':subject_refer_index,'Predict_result':Predict_result,'GradeRoll':GradeRoll})
+
+
+def prediction_ENGCE101(ENGCC304,FUNMA105,FUNSC101,GEBLC103):
+    Predict_result = predict_ENGCE101(ENGCC304,FUNMA105,FUNSC101,GEBLC103)
+    return Predict_result
+
+def prediction_ENGCE102(ENGCC304,FUNMA105,FUNSC101,GEBLC103):
+    Predict_result = predict_ENGCE102(ENGCC304,FUNMA105,FUNSC101,GEBLC103)
+    return Predict_result
+
+def prediction_ENGCE103(ENGCC304,ENGEE106,FUNSC101,GEBLC103):
+    Predict_result = predict_ENGCE103(ENGCC304,ENGEE106,FUNSC101,GEBLC103)
+    return Predict_result
+
+def prediction_ENGCE104(ENGCE102,ENGCE106,ENGEE106,FUNSC101):
+    Predict_result = predict_ENGCE104(ENGCE102,ENGCE106,ENGEE106,FUNSC101)
+    return Predict_result
+
+def prediction_ENGCE105(ENGCE102,ENGCE103,ENGCE106,ENGEL106,FUNMA105,FUNSC101):
+    Predict_result = predict_ENGCE105(ENGCE102,ENGCE103,ENGCE106,ENGEL106,FUNMA105,FUNSC101)
+    return Predict_result
+
+def prediction_ENGCE106(ENGCE104,ENGEL106,FUNSC101,GEBLC103):
+    Predict_result = predict_ENGCE106(ENGCE104,ENGEL106,FUNSC101,GEBLC103)
+    return Predict_result
+
+def prediction_ENGCE107(ENGCE102,ENGCE104,ENGCE106,FUNSC101):
+    Predict_result = predict_ENGCE107(ENGCE102,ENGCE104,ENGCE106,FUNSC101)
+    return Predict_result
+
+def prediction_ENGCE108(ENGCE105,ENGCE111,ENGCE112):
+    Predict_result = predict_ENGCE108(ENGCE105,ENGCE111,ENGCE112)
+    return Predict_result
+
+def prediction_ENGCE109(ENGCE104,ENGCE106,ENGEL106):
+    Predict_result = predict_ENGCE109(ENGCE104,ENGCE106,ENGEL106)
+    return Predict_result
+
+def prediction_ENGCE110(ENGCC304,ENGEL105,ENGEL106):
+    Predict_result = predict_ENGCE110(ENGCC304,ENGEL105,ENGEL106)
+    return Predict_result
+
+def prediction_ENGCE111(ENGCE103,ENGCE106,ENGEL106,GEBLC103):
+    Predict_result = predict_ENGCE111(ENGCE103,ENGCE106,ENGEL106,GEBLC103)
+    return Predict_result
+
+def prediction_ENGCE112(ENGCC304,ENGCE102,GEBLC103):
+    Predict_result = predict_ENGCE112(ENGCC304,ENGCE102,GEBLC103)
+    return Predict_result
+
+def prediction_ENGEL105(ENGCE106,ENGCE110,ENGEL106,FUNSC101,GEBLC103):
+    Predict_result = predict_ENGEL105(ENGCE106,ENGCE110,ENGEL106,FUNSC101,GEBLC103)
+    return Predict_result
+
+def prediction_ENGEL106(ENGCE102,ENGCE106,ENGEE106,FUNMA105,FUNSC101):
+    Predict_result = predict_ENGEL106(ENGCE102,ENGCE106,ENGEE106,FUNMA105,FUNSC101)
     return Predict_result
